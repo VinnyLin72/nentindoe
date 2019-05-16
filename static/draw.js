4627eaf86a8265a84fc5c07fbe3637b56fdec9b2
zane0916 <zwang7@stuy.edu>
2019-05-16 13:38:38 -0400

    added a draw fxn

--- static/draw.js
+++ static/draw.js
@@ -1,19 +1,13 @@
 var board = document.getElementById("slate");
 var draw = board.getContext("2d");
-var mode = 1; //if mode = 1, then in RectMode, if -1, then in DotMode
 
 draw.fillStyle = "#0000ff"; //makes blue
 draw.strokeStyle = "#0000ff"; 
 
-board.addEventListener('click', color);
+board.addEventListener('mousedown', color);
 
 var color = function() {
-    if (mode == 1) {
-	draw.fillRect(event.clientX, event.clientY, 10, 20);
-    }
-    else {
-	draw.beginPath();
-	draw.ellipse(event.clientX, event.clientY, 10, 10, 0, 360, false);
-	ctx.stroke();
-    }
+    draw.beginPath();
+    draw.ellipse(event.clientX, event.clientY, 1, 1, 0, 360, false);
+    ctx.stroke();
 }
