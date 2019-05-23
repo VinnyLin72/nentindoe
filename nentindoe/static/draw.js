558bf34ce2fcf63df37722cfa669d1d3ebf9aa51
mhowlader <mhowlader@stuy.edu>
2019-05-23 13:35:26 -0400

    addd draw js

--- nentindoe/static/draw.js
+++ nentindoe/static/draw.js
@@ -2,12 +2,33 @@ var board = document.getElementById("slate");
 var draw = board.getContext("2d");
 
 draw.fillStyle = "#0000ff"; //makes blue
-draw.strokeStyle = "#0000ff"; 
+draw.strokeStyle = "#0000ff";
 
-board.addEventListener('mousedown', color);
+// board.addEventListener('mousedown', color);
+//
+// var color = function() {
+//     draw.beginPath();
+//     draw.ellipse(event.clientX, event.clientY, 1, 1, 0, 360, false);
+//     ctx.stroke();
+// }
 
-var color = function() {
-    draw.beginPath();
-    draw.ellipse(event.clientX, event.clientY, 1, 1, 0, 360, false);
-    ctx.stroke();
+var mouseClicked = false, mouseReleased = true;
+
+document.addEventListener("click", onMouseClick, false);
+document.addEventListener("mousemove", onMouseMove, false);
+document.add
+
+function onMouseClick(e) {
+    mouseClicked = !mouseClicked;
+}
+
+function onMouseMove(e) {
+    if (mouseClicked) {
+        console.log("hello")
+        draw.beginPath();
+        draw.arc(e.clientX, e.clientY, 2, 0, Math.PI * 2, false);
+        draw.lineWidth = 5;
+        draw.strokeStyle = "#000";
+        draw.stroke();
+    }
 }
