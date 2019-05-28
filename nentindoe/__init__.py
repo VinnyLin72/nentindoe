71600ded21dacc6a9cd8811753b84134c4217886
Vincent Lin <vlin6@stuy.edu>
2019-05-28 17:34:04 +0000

    update

--- nentindoe/__init__.py
+++ nentindoe/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.6
 import os, csv, time, sqlite3, datetime, json
 
 from flask import Flask, redirect, url_for, render_template, session, request, flash, get_flashed_messages
